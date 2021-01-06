ffd66a5b88d3b1a6155f4adfd08f19d54b8fb198
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2021-01-06 10:14:56 +0100

    Add source code

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace SO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = new CultureInfo("en-US");
+            DateTime start = new DateTime(2020, 1, 2); // 2 January 2020
+            DateTime end = new DateTime(2020, 3, 4); // 4 March 2020
+
+            var daysInFirstMonth = DateTime.DaysInMonth(start.Year, start.Month) - start.Day;
+            Console.WriteLine(start.ToString("MMM", culture) + " (First Month): " + daysInFirstMonth.ToString("00"));  
+
+            DateTime current = start.AddMonths(1);
+            while (current <= end.AddMonths(-1))
+            {
+                var daysInCurrentMonth = DateTime.DaysInMonth(current.Year, current.Month);
+                Console.WriteLine(current.ToString("MMM", culture) + ": " + daysInCurrentMonth.ToString("00")); 
+
+                current = current.AddMonths(1);
+            }
+
+            var daysInLastMonth = end.Day;
+            Console.WriteLine(end.ToString("MMM", culture) + " (Last Month): " + daysInLastMonth.ToString("00"));
+        }
+    }
+}
